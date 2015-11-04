80795f079da07a10203a7d26d3bcd926720eab08
skulegirl <anna@egate.net>
2015-11-04 17:02:41 -0500

    Changes to support checklist feature

--- template.php
+++ template.php
@@ -118,3 +118,81 @@ function bdw2pointoh_preprocess_user_pass(&$variables) {
    $variables['intro_text'] = t('Forgotten your password? No problem. Enter your uesrname or email address below, and we\'ll send a one-time login link to the email addres you have listed in your account. From there you can reset your password to whatever you wish.');
 }
 
+/** Override theme_checklist_item from checklist module */
+function bdw2pointoh_checklist_item($variables) {
+  $link = $variables['link'];
+  $item = $variables['item'];
+  $list = $variables['list'];
+  $account = $variables['account'];
+  global $user;
+  static $js_added = FALSE;
+  $output = '<div class="checklist"><div id="checklistItem' . (!empty($list->instance_id) ? $list->instance_id : $list->clid) . '-' . $item->cliid . '" class="';
+
+  // Only get links and so on if the item is enabled.
+  if (($item->status == CHECKLIST_ENABLED) && !empty($list->instance_id)) {
+    $item->link = $link;
+    $options = (array) _checklist_item_callback_handler('display_options', $list, $item, $account);
+  }
+  else {
+    $options = array();
+  }
+
+  $options += array(
+    'div_class' => array(),
+    'query' => array(),
+    'show_admin' => FALSE, // This would show the link to an administrator if TRUE,
+  );
+  $options['div_class'][] = 'clMain';
+  if (!$link || ($item->status != CHECKLIST_ENABLED)) $options['div_class'][] = 'clDisabled';
+  $output .= implode(' ', $options['div_class']) . (!empty($item->checked_on) ? ' clChecked' : '') . '"></div>';
+
+  if ((!$link && !$list->completed) || !isset($options['path']) || (($user->uid != $account->uid) && !$options['show_admin'])) {
+    $output .= _checklist_clean($item->title);
+  }
+  else {
+    $path = $options['path'];
+    unset($options['path']);
+
+    // Try to bring the user back to this page if possible.
+    if (empty($options['query'])) {
+      $options['query'] = drupal_get_destination();
+    }
+    $output .= l($item->title, $path, $options);
+  }
+
+  if (!empty($item->checked_by) && $item->checked_by != $account->uid) {
+    $output .= '<span class="checked-by">' . t('Compled by !name', array('!name' => theme('username', array('account' => user_load($item->checked_by))))) . '</span>';
+  }
+
+  if ($item->description) {
+    $output .= ' </div><div id="checklistDetail' . (!empty($list->instance_id) ? $list->instance_id : $list->clid) . '-' . $item->cliid . '" class="checklistDetails">' . _checklist_clean($item->description, $item->format);
+  }
+
+  return $output . '</div>';
+}
+
+/* Override theme_checklist_lists from checklist module */
+
+function bdw2pointoh_checklist_lists($variables) {
+  $lists = $variables['lists'];
+  $account = $variables['account'];
+  drupal_add_css(drupal_get_path('module', 'checklist') . '/resources/checklist.css');
+
+  // Output the general description for checklists.
+  if ($output = variable_get('checklist_page_description', '')) {
+    $output = '<div id="checklist-page-description"><p>' . _checklist_clean($output['value'], $output['format']) . '</p></div>';
+  }
+  $output .= '<div class="checklists">';
+  $incomplete = FALSE;
+  foreach ($lists as $list) {
+    if (!$list->completed) $incomplete = TRUE;
+    $output .= theme('checklist_list', array('lists' => $list, 'account' => $account));
+  }
+  if ($incomplete && empty($_SESSION['checklist_required'])) {
+    $output .= checklist_continue_link();
+  }
+  $output .= '</div>';
+  return $output;
+}
+
+
